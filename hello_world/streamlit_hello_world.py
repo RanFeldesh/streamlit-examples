44991169a5a88f25e03dd1d41d0c6c8961714a6a
sertannavdann <sertannavdann@gmail.com>
2023-03-14 10:10:49 -0400

    Yolov5

--- hello_world/streamlit_hello_world.py
+++ hello_world/streamlit_hello_world.py
@@ -4,7 +4,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC')
+st.title('Uber whatever in NYC')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
